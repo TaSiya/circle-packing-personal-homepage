6f1563df9b9a7d069aae85a4f8e55fd691fe5026
Siya Makhalima <siya@uber5.com>
2021-10-15 23:50:51 +0200

    working on the project view, WIP

--- src/components/Project-sections.js
+++ src/components/Project-sections.js
@@ -4,9 +4,13 @@ import Projects from "./Projects"
 const Sections = ({section}) => {
   console.log("section", section)
   const viewProjects = section.projects.length > 0 ? section.projects.map((project, idx) => <Projects key={idx} project={project} />) : false
+  console.log("section.label", section.label, viewProjects)
+  if(!viewProjects) {
+    return ''
+  }
   return (
     <div className={section.name}>
-      <h4>{section.label}</h4>
+      <h2>{section.label}</h2>
       {viewProjects && viewProjects}
       <hr />
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Projects = ({project}) => {
-  console.log("project", project)
+  const {title, description, role, github, url, type} = project;
   return (
-    <div>
-    
+    <div className="card  btn-outline-secondary text-center" style={{width: 18 +'rem'}}>
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+      </div>
+      <div className="card-body">
+        <a href={url} className="card-link">Demo</a>
+        {github !== "#" ? <a href={github} className="card-link">github</a> : ''}
+      </div>
     </div>
   )
 }
